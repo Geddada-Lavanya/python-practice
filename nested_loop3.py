1b177c70edef3561b0d109f9fd0f007d303fea84
Geddada-Lavanya <lavanyageddada2@gmail.com>
2025-12-17 04:32:42 +0530

    Add files via upload

---
+++ nested_loop3.py
@@ -0,0 +1,11 @@
+#Processing Words in Sentences with Nested Loops
+def solution(sentence):
+    words = sentence.split(' ')
+    result = ''
+    for word in words:
+        if len(word) % 2 == 0:  # check if the length of word is even
+            for i in range(1, len(word), 2):  # loop over odd characters
+                result += word[i]
+    return result
+sentence=input()
+print(solution(sentence))
